6e19ed8e68c66b0fee2912705b0459db4a583810
Eatgrapes <Eatgrapesem232@qq.com>
2025-11-22 22:45:19 +0800

    Base

--- src/main/java/net/minecraft/util/Cartesian.java
+++ src/main/java/net/minecraft/util/Cartesian.java
@@ -70,7 +70,12 @@ public class Cartesian
 
         public Iterator<T[]> iterator()
         {
-            return (Iterator<T[]>)(this.iterables.length <= 0 ? Collections.singletonList((Object[])Cartesian.createArray(this.clazz, 0)).iterator() : new Cartesian.Product.ProductIterator(this.clazz, this.iterables));
+            if (this.iterables.length <= 0) {
+                List<T[]> list = Collections.singletonList(Cartesian.createArray(this.clazz, 0));
+                return list.iterator();
+            } else {
+                return new Cartesian.Product.ProductIterator(this.clazz, this.iterables);
+            }
         }
 
         static class ProductIterator<T> extends UnmodifiableIterator<T[]>
